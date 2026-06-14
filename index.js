dc25a11f72936ad6602bf55f31eb6260a76e905b
linelv <lelvislin@gmail.com>
2026-06-14 12:01:26 -0400

    Updated web light/dark mode to affect contact icons

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const root = document.documentElement;
 lightModeButton.onclick = function() {
     root.style.setProperty('--background-color', '#e8e8e8');
     root.style.setProperty('--text-color', '#0d0d0d');
+    root.style.setProperty('--icon-filter', 'invert()');
     lightModeButton.style.display = 'none';
     darkModeButton.style.display = 'inline';
     document.getElementById("header").style.filter = 'none';
@@ -13,6 +14,7 @@ lightModeButton.onclick = function() {
 darkModeButton.onclick = function() {
     root.style.setProperty('--background-color', '#0d0d0d');
     root.style.setProperty('--text-color', '#e8e8e8');
+    root.style.setProperty('--icon-filter', 'none');
     lightModeButton.style.display = 'inline';
     darkModeButton.style.display = 'none';
     document.getElementById("header").style.filter = 'invert()';
